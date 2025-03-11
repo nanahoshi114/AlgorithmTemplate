016ef0686fb03d31167400f5340a9e0b923eb998
nanahoshi114 <3196743624@qq.com>
2025-03-12 00:44:28 +0800

    fast cin

--- 快读.cpp
+++ 快读.cpp
@@ -13,3 +13,18 @@ T read(){
     }
     return sum * fl;
 }
+struct istream{
+	template<typename T>
+	istream operator>>(T &x){
+		x = 0;
+		T fl = 1;
+		int ch = getchar();
+		for(; !isdigit(ch); ch = getchar()){
+			if(ch == '-') fl = -1;
+		}
+		for(; isdigit(ch); ch = getchar()){
+			x = x * 10 + ch - '0';
+		}
+		return *this;
+	}	
+}cin;
