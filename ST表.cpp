243fb5500e10eaae94a8da8df852b53340f272ec
nanahoshi114 <3196743624@qq.com>
2025-04-01 20:58:00 +0800

    optimize

--- ST表.cpp
+++ ST表.cpp
@@ -5,9 +5,9 @@ template <typename T>
 class sparseTable{
     vector<vector<T>> ST;
     vector<T> lgn;
-    function<T(T, T)> func;
+    function<T(const T &, const T &)> func;
 public:
-    sparseTable(const vector<T> &org, function<T(T, T)> _func){
+    sparseTable(const vector<T> &org, function<T(const T &, const T &)> _func){
         func = _func;
         int sz = org.size() - 1;
         lgn.assign(sz + 5, 0);
@@ -15,19 +15,20 @@ public:
         for(int i = 3; i <= sz; i++){
             lgn[i] = lgn[i / 2] + 1;
         }
-        ST.assign(sz + 5, vector<T>(lgn[sz] + 5));
+        //ST.assign(sz + 5, vector<T>(lgn[sz] + 5));
+        ST.assign(lgn[sz] + 5, vector<T>(sz + 5));
         for(int i = 1; i <= sz; i++){
-            ST[i][0] = org[i];
+            ST[0][i] = org[i];
         }
         for(int j = 1; j <= lgn[sz]; j++){
             int pj = (1 << (j - 1));
             for(int i = 1; i + pj <= sz; i++){
-                ST[i][j] = func(ST[i][j - 1], ST[i + pj][j - 1]);
+                ST[j][i] = func(ST[j - 1][i], ST[j - 1][i + pj]);
             }
         }
     }
     T query(int l, int r){
         int pj = lgn[r - l + 1];
-        return func(ST[l][pj], ST[r - (1 << pj) + 1][pj]);
+        return func(ST[pj][l], ST[pj][r - (1 << pj) + 1]);
     }
 };
