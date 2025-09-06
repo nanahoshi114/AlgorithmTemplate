db0f3ec690aa9afd69ac2e9c386bb9e231164b6f
nanahoshi114 <3196743624@qq.com>
2025-09-06 12:36:08 +0800

    list

--- 树状数组套权值线段树.cpp
+++ 树状数组套权值线段树.cpp
@@ -135,3 +135,102 @@ public:
         return SegTree::query_kth_from_all(pl, pr, k);
     }
 };
+/* use std::list ver.
+class SegTree {
+    struct Node {
+        int val;
+        int ls, rs;
+    };
+    static const int range = int(1e9);
+    static int count;
+    static Node tree[MAXN];
+    int insert_new_node() {
+        tree[count] = {0, -1, -1};
+        return count++;
+    }
+    int query(int p, int l, int r, int fl, int fr) {
+        if (p == -1) return 0;
+        if (fl <= l && r <= fr) {
+            return tree[p].val;
+        }
+        int mid = (l + r) >> 1;
+        int res = 0;
+        if (fl <= mid) {
+            res += query(tree[p].ls, l, mid, fl, fr);
+        }
+        if (mid < fr) {
+            res += query(tree[p].rs, mid + 1, r, fl, fr);
+        }
+        return res;
+    }
+    void update(int p, int l, int r, int pos, int val) {
+        tree[p].val += val;
+        if (l == r) {
+            return ;
+        }
+        int mid = (l + r) >> 1;
+        if (pos <= mid) {
+            if (tree[p].ls == -1) {
+                tree[p].ls = insert_new_node();
+            }
+            update(tree[p].ls, l, mid, pos, val);
+        }
+        if (mid < pos) {
+            if (tree[p].rs == -1) {
+                tree[p].rs = insert_new_node();
+            }
+            update(tree[p].rs, mid + 1, r, pos, val);
+        }
+    }
+    static int query_kth_from_all(std::list<int>& p_vec_l, std::list<int>& p_vec_r, int l, int r, int k) {
+        int lsize = 0, nowsize = 0;
+        for (auto p : p_vec_r) {
+            nowsize += tree[p].val;
+            lsize += (tree[p].ls == -1 ? 0ll : tree[tree[p].ls].val);
+        }
+        for (auto p : p_vec_l) {
+            nowsize -= tree[p].val;
+            lsize -= (tree[p].ls == -1 ? 0ll : tree[tree[p].ls].val);
+        }
+        if (nowsize == 0) return -1;
+        if (l == r) return l;
+        int mid = (l + r) >> 1;
+        for (auto it = p_vec_l.begin(); it != p_vec_l.end(); ) {
+            int next_p = lsize >= k ? tree[*it].ls : tree[*it].rs;
+            if (next_p == -1) {
+                it = p_vec_l.erase(it);
+            } else {
+                *it = next_p;
+                ++it;
+            }
+        }
+        for (auto it = p_vec_r.begin(); it != p_vec_r.end(); ) {
+            int next_p = lsize >= k ? tree[*it].ls : tree[*it].rs;
+            if (next_p == -1) {
+                it = p_vec_r.erase(it);
+            } else {
+                *it = next_p;
+                ++it;
+            }
+        }
+        if (lsize >= k) {
+            return query_kth_from_all(p_vec_l, p_vec_r, l, mid, k);
+        }
+        return query_kth_from_all(p_vec_l, p_vec_r, mid + 1, r, k - lsize);
+    }
+public:
+    int root;
+    SegTree() {
+        root = insert_new_node();
+    }
+    int query(int l, int r) {
+        return query(root, 0, range, l, r);
+    }
+    void update(int pos, i64 val) {
+        update(root, 0, range, pos, val);
+    } 
+    static int query_kth_from_all(std::list<int>& p_vec_l, std::list<int>& p_vec_r, int k) {
+        return query_kth_from_all(p_vec_l, p_vec_r, 0, range, k);
+    }
+};
+*/
