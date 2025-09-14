7f55c367eece6b7762cbaba84a925c46ba79e280
nanahoshi114 <3196743624@qq.com>
2025-09-15 00:17:03 +0800

    optimize

--- ST表.cpp
+++ ST表.cpp
@@ -2,33 +2,55 @@
 #include <functional>
 
 template <typename T>
-class sparseTable{
-    std::vector<std::vector<T>> ST;
-    std::vector<T> lgn;
-    std::function<T(const T &, const T &)> func;
+class sparseTable {
+    std::vector<std::array<T, 30>> table;
+    std::vector<T> lg;
+    T (*func)(const T, const T);
 public:
-    sparseTable(const std::vector<T> &org, std::function<T(const T &, const T &)> _func){
-        func = _func;
-        int sz = org.size() - 1;
-        lgn.assign(sz + 5, 0);
-        lgn[1] = 0, lgn[2] = 1;
-        for(int i = 3; i <= sz; i++){
-            lgn[i] = lgn[i / 2] + 1;
+    sparseTable(const std::vector<T>& arr, T (*func)(const T, const T)) : func(func), table(arr.size()), lg(arr.size()) {
+        lg[1] = 0;
+        int n = arr.size() - 1;
+        for (int i = 2; i <= n; ++i) {
+            lg[i] = lg[i / 2] + 1;
         }
-        //ST.assign(sz + 5, vector<T>(lgn[sz] + 5));
-        ST.assign(lgn[sz] + 5, std::vector<T>(sz + 5));
-        for(int i = 1; i <= sz; i++){
-            ST[0][i] = org[i];
+        for (int i = 1; i <= n; ++i) {
+            table[i][0] = arr[i];
         }
-        for(int j = 1; j <= lgn[sz]; j++){
-            int pj = (1 << (j - 1));
-            for(int i = 1; i + pj <= sz; i++){
-                ST[j][i] = func(ST[j - 1][i], ST[j - 1][i + pj]);
+        for (int j = 1; j <= lg[n]; ++j) {
+            for (int i = 1; i + (1 << j) - 1 <= n; ++i) {
+                table[i][j] = func(table[i][j - 1], table[i + (1 << (j - 1))][j - 1]);
             }
         }
     }
-    T query(int l, int r){
-        int pj = lgn[r - l + 1];
-        return func(ST[pj][l], ST[pj][r - (1 << pj) + 1]);
+    T query(int l, int r) {
+        int k = lg[r - l + 1];
+        return func(table[l][k], table[r - (1 << k) + 1][k]);
+    }
+};
+template <typename T>
+class sparseTable {
+    std::vector<T> table[30];
+    std::vector<T> lg;
+    T (*func)(const T, const T);
+public:
+    sparseTable(const std::vector<T>& arr, T (*func)(const T, const T)) : func(func), lg(arr.size()) {
+        for (auto& v : table) v.resize(arr.size());
+        lg[1] = 0;
+        int n = arr.size() - 1;
+        for (int i = 2; i <= n; ++i) {
+            lg[i] = lg[i / 2] + 1;
+        }
+        for (int i = 1; i <= n; ++i) {
+            table[0][i] = arr[i];
+        }
+        for (int j = 1; j <= lg[n]; ++j) {
+            for (int i = 1; i + (1 << j) - 1 <= n; ++i) {
+                table[j][i] = func(table[j - 1][i], table[j - 1][i + (1 << (j - 1))]);
+            }
+        }
+    }
+    T query(int l, int r) {
+        int k = lg[r - l + 1];
+        return func(table[k][l], table[k][r - (1 << k) + 1]);
     }
 };
