f9976826332a60cf17da251a3aca40d396cd5a47
nanahoshi114 <3196743624@qq.com>
2025-09-20 10:45:23 +0800

    optimize

--- 线性素数筛.cpp
+++ 线性素数筛.cpp
@@ -1,44 +1,35 @@
 #include <iostream>
 #include <vector>
-#include <cstring>
-#include <cmath>
 #include <bitset>
-
-const int M = 1e8 + 1;
+constexpr int M = 1e8 + 1;
 using i64 = long long;
-struct Prime{
+struct Prime {
     std::bitset<M> prime_flag;
     std::vector<int> prime_list;
-    int size;
-    Prime(int _size) : size(_size){
-        prime_flag = ~prime_flag;
+    Prime() {
+        prime_flag.set();
         prime_flag[1] = 0;
-        for(int i = 2; i <= size; i++){
+        for(int i = 2; i < M; i++){
             if(prime_flag[i]){
                 prime_list.push_back(i);
             }
             for(auto j : prime_list){
-                if(i * j > size) break;
+                if(i * j >= M) break;
                 prime_flag[i * j] = 0;
                 if(i % j == 0) break;
             }
         }
     }
-    int operator[](int ind){
-        return prime_list[ind];
-    }
-    bool is_prime(int x){
-        return prime_flag[x];
-    }
-} prime(M - 1);
+} prime;
 int main(){
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+    std::cout.tie(nullptr);
     int n, q;
     std::cin >> n >> q;
-    Prime prime(n);
     while(q--){
         int t;
         std::cin >> t;
-        std::cout << prime[t - 1] << '\n';
+        std::cout << prime.prime_list[t - 1] << '\n';
     }
     return 0;
 }
